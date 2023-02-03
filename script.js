48d2f5e62bd9fcbdc2e1a21cbfb369fe7b7f1051
murat <slipstream11@yandex.com>
2023-02-03 18:40:24 +0300

    fix commit

--- script.js
+++ script.js
@@ -8,16 +8,12 @@ const submitButton = document.getElementById('submit');
 const rating = document.getElementById('rating');
 const rates = document.querySelectorAll('.btn');
 
-// submitButton.addEventListener('click', () => {
-//     thanksContainer.classList.remove('hidden');
-//     mainContainer.style.display = 'none'
-// })
-
 thanksContainer.addEventListener('click', function () {
     mainContainer.style.display = 'block';
     thanksContainer.classList.add('hidden');
     rating.innerHTML = '';
     document.querySelector('.gray').textContent = 'Please Select Number'
+    //Short fix problem for now.
 })
 
 rates.forEach(rate => {
@@ -26,11 +22,10 @@ rates.forEach(rate => {
     })
     console.log(rate.innerHTML, rate);
 })
-
+//If no number clicked It wont submit. But second time when rate it submit somehow, I will be pleased if you give feedback for solve this. For now  when second rate no number selected, it writes 'please select number'. 
 rates.forEach(occurence => {
     let id = occurence.getAttribute('id');
     occurence.addEventListener('click', function () {
-        console.log('A button with ID ' + id + ' was clicked!')
 
         submitButton.addEventListener('click', () => {
             thanksContainer.classList.remove('hidden');
