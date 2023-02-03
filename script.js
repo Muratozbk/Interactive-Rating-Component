45b57b8c6152c9ab52cc24814434e2e3968bf0b7
murat <slipstream11@yandex.com>
2023-02-03 18:29:55 +0300

    finish commit

--- script.js
+++ script.js
@@ -4,28 +4,37 @@ const mainContainer = document.querySelector('.main-container');
 const thanksContainer = document.querySelector('.thank-you');
 
 const submitButton = document.getElementById('submit');
-const rateAgain = document.getElementById('rate-again');
 
 const rating = document.getElementById('rating');
 const rates = document.querySelectorAll('.btn');
 
-submitButton.addEventListener('click', () => {
-    thanksContainer.classList.remove('hidden');
-    mainContainer.style.display = 'none'
-})
-
-rateAgain.addEventListener('click', () => {
-    mainContainer.style.display = 'block';
-    thanksContainer.classList.add('hidden')
-})
-
-rates.forEach((rate) => {
-    rate.addEventListener('click', () => {
-        rating.innerHTML = rate.innerHTML
-    })
-})
+// submitButton.addEventListener('click', () => {
+//     thanksContainer.classList.remove('hidden');
+//     mainContainer.style.display = 'none'
+// })
 
 thanksContainer.addEventListener('click', function () {
     mainContainer.style.display = 'block';
-    thanksContainer.classList.add('hidden')
+    thanksContainer.classList.add('hidden');
+    rating.innerHTML = '';
+    document.querySelector('.gray').textContent = 'Please Select Number'
 })
+
+rates.forEach(rate => {
+    rate.addEventListener('click', () => {
+        rating.innerHTML = (rate.innerHTML)
+    })
+    console.log(rate.innerHTML, rate);
+})
+
+rates.forEach(occurence => {
+    let id = occurence.getAttribute('id');
+    occurence.addEventListener('click', function () {
+        console.log('A button with ID ' + id + ' was clicked!')
+
+        submitButton.addEventListener('click', () => {
+            thanksContainer.classList.remove('hidden');
+            mainContainer.style.display = 'none'
+        })
+    });
+});
